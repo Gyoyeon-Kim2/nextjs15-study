4fecb2910cac0654620031228c979ee83a582d49
Gyoyeon Kim <asjni516@cu.ac.kr>
2025-07-05 00:04:50 +0900

    55 - Dynamic Rendering

--- rendering-demo/src/app/about/page.tsx
+++ rendering-demo/src/app/about/page.tsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { cookies } from "next/headers";
 
-export default function AboutPage() {
-    const [name, setName] = useState("");
+export default async function AboutPage() {
+  const cookiesStore = await cookies();
+  const theme = cookiesStore.get("theme");
+  console.log(theme);
   console.log("About server component")
   return <h1>About page{new Date().toLocaleTimeString()}</h1>;
   
